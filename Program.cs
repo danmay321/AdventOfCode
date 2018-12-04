b7bdbf2c957d4aaa2ff12dad826bf3c7a6fa3e2c
danmay321 <daniel.may111@gmail.com>
2018-12-03 22:04:04 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeDayOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+            {
+                List<string> values = File.ReadLines("Values.txt").ToList();
+                int finalValue = 0;
+                finalValue = values.Sum(x => int.Parse(x) + finalValue);
+        }
+    }
+}
